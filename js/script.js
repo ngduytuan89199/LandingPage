450e591042bbee26f9df9b05a36df40b91c959e5
ngduytuan89199 <ngduytuan8199@gmail.com>
2020-08-17 18:02:33 +0700

    add new project

---
+++ js/script.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+    $('.header-menu a').click(function (e) {
+        $('.current').removeClass('current');
+        $('.active').removeClass('active');
+        var posClick = $(this).attr('href');
+        $("[href='" + posClick + "']").addClass('current');
+        $("[href='" + posClick + "']").addClass('active');
+    });
+
+    $('.parallax').ripples({
+        dropRadius: 1,
+        perturbance: 0.1,
+        resolution: 400,
+    });
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 80) {
+            $("#product-gallery").addClass("slideup")
+        }
+        else {
+            $("#product-gallery").removeClass("slideup")
+        }
+
+        if (scroll > 1200) {
+            $("#About").addClass("slideright")
+        }
+        else {
+            $("#About").removeClass("slideright")
+        }
+    }) 
+
+});
